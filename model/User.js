b947a2b4838d8947474470fc80a2b9d04e810557
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-07-24 10:24:46 +0530

    ADD:user schema

---
+++ model/User.js
@@ -0,0 +1,10 @@
+export default class users{
+   constructor(name,address,phone,pincode,state){
+       this.name = name;
+       this.address = address;
+       this.phone = phone;
+       this.pincode = pincode;
+       this.state = state;
+   };
+}
+
